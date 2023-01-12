c8a8dfbb0865c1747a1e12ef3e1afba220604c52
4halavandala <1terrormachine1@gmail.com>
2023-01-12 18:56:38 +0300

    added test for emailvalidator

--- src/main/java/ru/halavandala/jwtauthentication/Validation/EmailValidator.java
+++ src/main/java/ru/halavandala/jwtauthentication/Validation/EmailValidator.java
@@ -4,18 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailValidator {
-    private static final String EMAIL_PATTERN =
-           "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
-                   "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
+    public boolean isValid(String email)
+    {
+        if (email == null) {
+            return false;
+        }
 
-    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-    private Matcher matcher;
-
-
-    public boolean validate(final String hex) {
-        matcher = pattern.matcher(hex);
-
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
         return matcher.matches();
     }
 }
